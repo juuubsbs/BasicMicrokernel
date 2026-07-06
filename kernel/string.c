6a6b0f2fe1ab712b14b4785ace61e574479b770a
PandaNico2 <isadorademello2020@gmail.com>
2026-07-05 21:43:45 -0300

    correção das funções de string (memcpy, strcmp, strcpy, strlen) usadas pelo treefs

--- kernel/string.c
+++ kernel/string.c
@@ -7,3 +7,36 @@ void *memset(void *s, int c, uint64_t n)
         *p++ = c;
     return s;
 }
+
+void *memcpy(void *dest, const void *src, uint64_t n)
+{
+    uint8_t *d = dest;
+    const uint8_t *s = src;
+    while (n--)
+        *d++ = *s++;
+    return dest;
+}
+
+int strcmp(const char *a, const char *b)
+{
+    while (*a && (*a == *b)) {
+        a++;
+        b++;
+    }
+    return (unsigned char)*a - (unsigned char)*b;
+}
+
+char *strcpy(char *dest, const char *src)
+{
+    char *ret = dest;
+    while ((*dest++ = *src++));
+    return ret;
+}
+
+uint64_t strlen(const char *s)
+{
+    uint64_t n = 0;
+    while (s[n])
+        n++;
+    return n;
+}
